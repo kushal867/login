91804b1b9d6c9cb7959651497804e4d82a3740e4
Kushal <your_email@example.com>
2025-09-30 17:54:03 +0545

    Updated

--- users/views.py
+++ users/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from users.forms import LoginForm, RegisterForm
 
-# Create your views here.
-test
+
+def user_login(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user:
+                login(request, user)
+                return redirect('/admin')
+    return render(request, 'login.html', {"form": form})
+
+
+def user_defined_login(request):
+    if request.user.is_authenticated:
+        return redirect('/admin')
+
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('/admin')
+
+    return render(request, 'user_login.html', {"form": form})
+
+
+def register(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('/userlogin')
+    return render(request, 'register.html', {"form": form})
